d150ae0a5606102ea46378cc39d5bbf796c1b325
unknown <almir.semic@yahoo.com>
2022-06-23 22:58:26 +0200

    empty string boot project initialised

---
+++ src/main/java/com/example/almirtask/AlmirTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.almirtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmirTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmirTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/almirtask/AlmirTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.almirtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmirTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
